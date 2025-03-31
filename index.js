59b119a3ab5eb6ce46eb669a5c3f3d3e5c64eb99
Dixit soni <dixitsoni424@gmail.com>
2025-04-01 01:27:56 +0530

    update code

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
-
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
+const cors = require("cors");
+// const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRouter = require("./src/routes/userController");
+const productRouter = require("./src/routes/productController");
+const path = require('path')
+dotenv.config();
+app.use(cors());
+// const authRoute = require("./routes/auth");
+// const userRoute = require("./routes/users");
+// const postRoute = require("./routes/posts");
+// const categoryRoute = require("./routes/categories");
+// const multer = require("multer");
+// const path = require("path");
+app.get('/', (req, res) => {
+  res.send('welcome to my api')
 })
+const db = require('./src/db/db')
+db()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(authRouter)
+app.use(productRouter)
+
+
+
 
 app.listen(8080, () => {
-    console.log(`server is running on port 8080`);
+  console.log(`server is running on port 8080`);
 });
