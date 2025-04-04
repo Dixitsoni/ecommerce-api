a6dccc5556db2d001b724cbbfa43f96239148497
Dixit soni <dixitsoni424@gmail.com>
2025-04-04 18:16:28 +0530

    update api

--- src/routes/orderController.js
+++ src/routes/orderController.js
@@ -42,7 +42,7 @@ orderRouter.get('/order/:userId', async (req, res) => {
     const { userId } = req.params;
 
     try {
-        const orders = await Order.find({ userId }).populate('products.productId');
+        const orders = await Order.findOne({ userId }).populate('products.productId');
         res.status(200).json(orders);
     } catch (error) {
         res.status(500).json({ message: error.message });
